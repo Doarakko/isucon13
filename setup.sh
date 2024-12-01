c68e7543fafd04a230d5303baa665a6a7bd019e9
Doarakko <2wua4nlyi@gmail.com>
2024-12-01 21:41:55 +0900

    cp mysql and nginx config when setup

--- setup.sh
+++ setup.sh
@@ -2,8 +2,12 @@ USER:=isucon
 
 mkdir /home/$USER/log
 mkdir /home/$USER/tools && cd /home/$USER/tools
+
 mkdir -p /home/$USER/mysql/mysql.conf.d
+cp /etc/mysql/mysql.conf.d/mysqld.cnf /home/$USER/mysql/mysql.conf.d/mysqld.cnf
+
 mkdir /home/$USER/nginx
+cp /etc/nginx/nginx.conf /home/$USER/nginx/nginx.conf
 
 # pt-query-digest
 wget https://github.com/percona/percona-toolkit/archive/refs/tags/v3.5.5.tar.gz
